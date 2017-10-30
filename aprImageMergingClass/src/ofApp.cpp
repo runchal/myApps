d45f9c4edaa506fb554930a89528514351eb54e5
Amit Runchal <amitrunchal@gmail.com>
2017-10-29 22:36:05 -0400

    New homework for image merging

--- aprImageMergingClass/src/ofApp.cpp
+++ aprImageMergingClass/src/ofApp.cpp
@@ -18,7 +18,17 @@ void ofApp::setup(){
      ton of floating point numbers. I'll create an array of integer numbers, divide by the total.
      */
     
-    int pixelSum[ 360 * 240 * 3]; // this creates an array of ints that represent the number of pixel colors we would store. if this was black and white I suppose we could use just one pixel thing for grey, but this is basically creating a line of numbers? actually i realized i have no clue what is going on here. After running a cout it appears that it is creating an array of a specific size with values stored to 0
+    int pixelSum[ 360 * 240 * 3];
+    
+    //detroit
+    for (int i = 0; i<360*240*3; i++){
+        pixelSum[i]=0;
+    }
+    
+    
+    // this creates an array of ints that represent the number of pixel colors we would store. if this was black and white I suppose we could use just one pixel thing for grey, but this is basically creating a line of numbers? actually i realized i have no clue what is going on here. After running a cout it appears that it is creating an array of a specific size with values stored to 0
+    
+    dir.sort();
     
     for (int i = 0; i < dir.size(); i++){
         cout << "loading: " << dir.getPath(i) << endl; // this is not returning things that I want it to return.
@@ -26,17 +36,49 @@ void ofApp::setup(){
         temp.setUseTexture(false); // this takes the temp ofImage thing and makes it not store the texture
         temp.load(dir.getPath(i)); // this will load in each image from the path.
         
+        /*
+         ofImage has texture, and texture uploads things to graphic cards and turning it off will make it not load to the graphic card and make it run faster
+         */
+        
         /* now we want to add to the pixel sum (the value of each first pixel) by creating a for loop
          that takes the red green and blue image, going to grab the data
          */
+        
+        for (int j = 0; j<360; j++){
+            for (int k = 0; k < 240; k++){ //Zach wants to point out that this is where you can get a lot of bugs in your code, when you have a for loop inside of for loop
+                ofColor c = temp.getColor(j, k); // this is going though every single pixel and getting the color from that pixel. j,k are the pixel coordinates and what it's doing is getting the file from the folder associated with temp, and then getting the pixel color
+                int index = (k * 360 + j)*3; // zach has this as funky math. As he explains, this is a one-dimensional array, we need to figure out where we are inside that one dimensional array, we are in a two dimensional for loop.
+                pixelSum[index] += c.r;
+                pixelSum[index+1] += c.g;
+                pixelSum[index+2] += c.b;
+                
+                
+            }
+        }
+        
+        cout << "pixel sum of red for pixel 0 " << pixelSum[0] << endl;
+        
     }
     
-        // is it normal to feel like an idiot all the time when programming or is it just m
+    for (int i = 0; i<360; i++){
+        for (int j = 0; j < 240; j++){
+            ofColor c; // this initializes this
+            int index = (j*360+i)*3;
+            c.r = (float)pixelSum[index] / (float)dir.size();
+            c.g = (float)pixelSum[index+1] / (float)dir.size();
+            c.b = (float)pixelSum[index+2] / (float)dir.size();
+            noahMix.setColor(i,j, c);
+            
+      
+        
+        }
+    }
     
-    /*
-     ofImage has texture, and texture uploads things to graphic cards and turning it off will make it not load to the graphic card and make it run faster
-     
-     */
+    noahMix.update();
+        // is it normal to feel like an idiot all the time when programming or is it just me
+    
+    
+    
     
 }
 
@@ -48,6 +90,7 @@ void ofApp::update(){
 //--------------------------------------------------------------
 void ofApp::draw(){
 
+    noahMix.draw(0,0);
 }
 
 //--------------------------------------------------------------
